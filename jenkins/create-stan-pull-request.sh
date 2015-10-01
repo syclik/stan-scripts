d7e1e3847462b0100b30328ccb733830c2f5f1c0
Daniel Lee <bearlee@alum.mit.edu>
2015-10-01 00:22:04 -0400

    updating scripts again

--- jenkins/create-stan-pull-request.sh
+++ jenkins/create-stan-pull-request.sh
@@ -52,32 +52,32 @@ if [ "$original_commit_hash"=="$math_commit_hash" ]; then
   exit 0
 fi
 
-# ########################################
-# ## Create GitHub issue
-# ########################################
+########################################
+## Create GitHub issue
+########################################
 
-# issue="{ 
-#   \"title\": \"Update submodule for the Stan Math Library\",
-#   \"body\":  \"The Stan Math Library develop branch has been updated.\nUpdate the submodule to ${math_commit_hash}.\" }"
+issue="{ 
+  \"title\": \"Update submodule for the Stan Math Library\",
+  \"body\":  \"The Stan Math Library develop branch has been updated.\nUpdate the submodule to ${math_commit_hash}.\" }"
 
-# response=$(eval curl --include --user \"$github_user:$github_token\" --request POST --data \'$issue\' https://api.github.com/repos/stan-dev/stan/issues)
-
-
-# if ! curl_success "${response}"; then
-#   _msg="
-# Error creating pull request:
-# ----------------------------
-# $data
+response=$(eval curl --include --user \"$github_user:$github_token\" --request POST --data \'$issue\' https://api.github.com/repos/stan-dev/stan/issues)
 
 
-# Response:
-# ---------
-# $response
-# "
-#   exit 1
-# fi
+if ! curl_success "${response}"; then
+  _msg="
+Error creating pull request:
+----------------------------
+$data
 
-# parse_github_issue_number "${response}"
+
+Response:
+---------
+$response
+"
+  exit 1
+fi
+
+parse_github_issue_number "${response}"
 
 
 ########################################
@@ -86,7 +86,6 @@ fi
 ## - Update the Math Library to develop
 ## - Commit and push
 ########################################
-github_issue_number=1625
 git checkout -b feature/issue-${github_issue_number}-update-math
 pushd lib/stan_math > /dev/null
 git checkout ${math_commit_hash}
